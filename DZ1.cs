772ebe51ad850668b3c8c27d8304efb2abd7aab2
Elijah <eligahbunn@gmail.com>
2022-08-20 17:34:12 +0300

    commit 1

---
+++ DZ1.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите переменную х:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите переменную у:");
+double y = double.Parse(Console.ReadLine());
+if (y * y == x)
+{
+    Console.WriteLine("х - квадрант числа у");
+}
+
+else if (y * y != x)
+{
+    Console.WriteLine("х - не квадрант числа у");
+}
+
