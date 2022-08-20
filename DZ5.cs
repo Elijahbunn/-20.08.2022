837a08c7dc49f4984d8ec59407996b2f726692f0
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-08-20 19:05:55 +0300

    commit1

---
+++ DZ5.cs
@@ -0,0 +1,17 @@
+﻿// int[] numbers = {1..15};
+// int size = 16;
+// int index = 0;
+// int d = numbers[index] % 2;
+
+int N = 15;
+int current = 1;
+int d = current % 2;
+
+while (current <= N)
+{
+    if (d == 0)
+    {
+        Console.WriteLine (" " + current);
+    }
+    current = current + 1;
+}
