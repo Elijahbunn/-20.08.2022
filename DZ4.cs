5be14dd42847c1d87a053e6893f5c4443a408a7d
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-08-20 18:23:53 +0300

    commit1

---
+++ DZ4.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+double x = double.Parse(Console.ReadLine());
+double d = x % 2;
+
+if (d == 0)
+{
+    Console.WriteLine("Число четное");
+}
+
+else if (d != 0)
+{
+    Console.WriteLine("Число нечетное");
+}
