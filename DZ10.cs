ef6ac99a6b02cab8dcf2c0f055142d803a91012a
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-08-23 21:57:20 +0300

    commit1

---
+++ DZ10.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("Введите проверяемое число: ");
+int current = int.Parse(Console.ReadLine());
+
+if (current < 100)
+{
+Console.Write("Число отсутствует");
+}
+if (100 <= current && current < 1000)
+{
+int a1 = current % 10;
+Console.Write(a1);
+}
+if (1000 <= current && current < 10000)
+{
+int a1 = (current / 10) % 10;
+Console.Write(a1);
+}
+if (10000 <= current && current < 100000)
+{
+int a1 = (current / 100) % 10;
+Console.Write(a1);
+}
+if (100000 <= current && current < 1000000)
+{
+int a1 = (current / 1000) % 10;
+Console.Write(a1);
+}
+if (1000000 <= current && current < 10000000)
+{
+int a1 = (current / 10000) % 10;
+Console.Write(a1);
+}
+if (10000000 <= current && current < 100000000)
+{
+int a1 = (current / 100000) % 10;
+Console.Write(a1);
+}
+if (100000000 <= current && current < 1000000000)
+{
+int a1 = (current / 1000000) % 10;
+Console.Write(a1);
+}
+if (1000000000 <= current && current < 1000000000)
+{
+int a1 = (current / 10000000) % 10;
+Console.Write(a1);
+}
