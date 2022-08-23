e8c049e52f2d075c0960d9e86915f17e7348dba4
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-08-23 21:56:26 +0300

    commit1

---
+++ DZ7.cs
@@ -0,0 +1,14 @@
+﻿Random rnd = new Random();
+int number = rnd.Next(10, 99);
+Console.WriteLine(number);
+
+int a1 = number / 10;
+int b1 = number % 10;
+if (a1 > b1)
+{
+    Console.WriteLine(a1);
+}
+else if (a1 < b1)
+{
+    Console.WriteLine(b1);
+}
