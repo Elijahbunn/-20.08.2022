e114d88db31f160f322a3056ef4587c70b7e36f2
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-08-23 21:57:01 +0300

    commit1

---
+++ DZ9.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите проверяемое число: ");
+int current = int.Parse(Console.ReadLine());
+Console.Write("Введите делитель: ");
+int del = int.Parse(Console.ReadLine());
+
+int kiss = current % del;
+
+if (kiss == 0)
+{
+Console.Write("Проверяемое число кратно заданному.");
+}
+
+else if (kiss != 0)
+{
+Console.Write("Остаток равен: " + kiss);
+}
